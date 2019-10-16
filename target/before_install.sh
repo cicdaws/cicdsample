97d7ac9ce4fec61ddf518f66addf126ee41d9b78
cicdaws <55483376+cicdaws@users.noreply.github.com>
2019-10-16 20:50:41 +0530

    Update before_install.sh

--- target/before_install.sh
+++ target/before_install.sh
@@ -2,4 +2,4 @@ cd /home/ubuntu/
 sudo systemctl stop wildfly
 
 cd /opt/wildfly/wildfly-16.0.0.Final/wildfly-16.0.0.Final/standalone/deployments
-rm CICDSample-0.0.1-SNAPSHOT.war
+sudo rm CICDSample-0.0.1-SNAPSHOT.war
